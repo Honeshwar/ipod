cf12de402780630e978ef55a1827a2aa3cc3dcf5
Honeshwar Thakur <st566654@gmail.com>
2023-02-18 18:23:32 +0530

    create menu and up/down functionality

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,7 +1,71 @@
 import React from 'react'
 
 export default function Buttons() {
+  const all = document.querySelectorAll('#menu li');
+  console.log(all);
+  let count=0;
+  let Default=true;
+  if(Default){
+    all[count].style.boxShadow="0 0 5px 1px black";
+    all[count].style.backgroundColor="blue";
+    all[count].style.color="white";
+    Default=false;
+  }
+  function goUpHandler(){
+    count--;
+    if(count<0){
+      count=3;
+    }
+    all[count].style.backgroundColor="blue";
+    all[count].style.color="white";
+    all[count].style.boxShadow="0 0 5px 1px black";
+    // all[count].style.boxShadow="0 0 10px 1px gray";
+   
+   
+if(count>-1){
+  let index=count;
+  if(count===3){
+     index=-1;
+  }
+
+  all[index+1].style.boxShadow="none";
+  all[index+1].style.backgroundColor="lightgray";
+  all[index+1].style.color="black";
+
+}
+  }
+
+   function goDownHandler(){
+    count++;
+    if(count>3){
+      count=0;
+    }
+    all[count].style.backgroundColor="blue";
+    all[count].style.color="white";
+    all[count].style.boxShadow="0 0 5px 1px black";
+   
+    if(count>-1){
+      let index=count;
+      if(count===0){
+         index=4;
+      }
+    
+      all[index-1].style.boxShadow="none";
+      all[index-1].style.backgroundColor="lightgray";
+      all[index-1].style.color="black";
+    
+    }
+
+  }
   return (
-    <div id='buttons'>Buttons</div>
+    <div id='buttons'>
+      
+        <button onClick={goUpHandler}>_Up_</button>
+        <button >Menu</button>
+        <button>Menu</button>
+        <button onClick={goDownHandler}>Down</button>
+        <button >Okay</button>
+      
+    </div>
   )
 }
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,14 +1,15 @@
 import React from 'react'
 
 export default function Menu() {
+ 
   return (
     <div id='menu'>
-        <ul>
-            <li>Game</li>
-            <li>Music</li>
-            <li>over Flo</li>
-            <li>Setting</li>
-        </ul>
+       <label>IPOD</label>
+          <li>Game</li>
+          <li>Music</li>
+          <li>over Flo</li>
+          <li>Setting</li>
+        
     </div>
-  )
+  );
 }
